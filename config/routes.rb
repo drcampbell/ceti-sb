9a3cc6cd25d954728c76856fad7a5fd78b1fe233
David Campbell <d.cam09@gmail.com>
2015-05-21 17:24:23 -0400

    change

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   resources :claims
   resources :locations
 
-  root 'application#hello'#to: 'static_pages#home'
+  root to: 'static_pages#home'
   get    'help'    => 'static_pages#help'
   get    'about'   => 'static_pages#about'
   get    'contact' => 'static_pages#contact'

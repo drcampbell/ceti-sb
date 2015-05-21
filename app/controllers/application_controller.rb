8b07783cda2ace122a2159e2f5032463c4e50a44
David Campbell <d.cam09@gmail.com>
2015-05-21 16:39:02 -0400

    change root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
 
+  def hello
+    render text: "hello cruel world"
+  end
+  
   protected
 
   def configure_permitted_parameters

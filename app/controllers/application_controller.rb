faed38357ef248cdc2b349599c4abd1f7748002a
David Campbell <d.cam09@gmail.com>
2015-06-15 19:56:37 -0400

    Remove token authenticaiton from AppController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   #before_filter :authenticate_user_from_token!
-  acts_as_token_authentication_handler_for User
+  #acts_as_token_authentication_handler_for User, 
+    #unless: lambda { |StaticPagesController| }
   before_filter :authenticate_user!
   #before_filter :configure_permitted_parameters, if: :devise_controller?
   include ActionController::MimeResponds

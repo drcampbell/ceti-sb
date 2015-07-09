466226befce508f03d4d59fc28e1f35f4e538406
David Campbell <d.cam09@gmail.com>
2015-07-09 11:38:55 -0400

    UPdate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150706151555) do
+ActiveRecord::Schema.define(version: 20150708191229) do
 
   create_table "badges", force: :cascade do |t|
     t.string   "file"

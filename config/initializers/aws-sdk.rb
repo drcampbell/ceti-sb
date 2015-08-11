ac4f8dde0e73d45b1ef575150b3fb13d9a74b14c
David Campbell <d.cam09@gmail.com>
2015-08-11 17:01:46 -0400

    Update production environment

--- config/initializers/aws-sdk.rb
+++ config/initializers/aws-sdk.rb
@@ -2,5 +2,3 @@
 Aws.config[:log_level] = :debug
 
 Aws.config[:credentials] = Aws::Credentials.new(ENV["AWS_ACCESS_KEY"], ENV["AWS_SECRET_KEY"])
-
-Aws.config[:mail_credentials] = Aws::Credentials.new(ENV["SENDGRID_USERNAME"], ENV["SENDGRID_PASSWORD"])

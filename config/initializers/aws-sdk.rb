b323972867e0c3d0ca9f73bbf9c4bf65fe482e67
David Campbell <d.cam09@gmail.com>
2015-08-12 18:03:46 -0400

    Update production environment

--- config/initializers/aws-sdk.rb
+++ config/initializers/aws-sdk.rb
@@ -5,10 +5,10 @@ creds = Aws::Credentials.new(ENV["AWS_ACCESS_KEY"], ENV["AWS_SECRET_KEY"])
 mcreds = Aws::Credentials.new(ENV["SENDGRID_USERNAME"], ENV["SENDGRID_PASSWORD"])
 Aws.config[:credentials] = creds
 
-ActionMailer::Base.add_delivery_method(:aws_sdk, Aws::SES::Client,
+ActionMailer::Base.add_delivery_method :aws_sdk, Aws::SES::Client,
 	:credentials: mcreds,
 	region: "us-west-2"
-	)
+	
 ActionMailer::Base.add_delivery_method :ses, Aws::SES::Client,
 	:access_key_id	=> ENV["SENDGRID_USERNAME"],
 	:secret_access_key => ENV["SENDGRID_PASSWORD"]

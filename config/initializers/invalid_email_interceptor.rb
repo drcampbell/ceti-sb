ed3c38f8d53471c127d1f1d4e909cccec8fa5b6a
David Campbell <d.cam09@gmail.com>
2015-08-19 16:06:33 -0400

    Add email responses controller

--- config/initializers/invalid_email_interceptor.rb
+++ config/initializers/invalid_email_interceptor.rb
@@ -1,7 +1,8 @@
 class BouncedEmailInterceptor
-
+	enum response_type: [ :bounce, :complaint, :ooto ]
+	
 	def self.delivering_email(message)
-		enum response_type: [ :bounce, :complaint, :ooto ]
+		
 		if (EmailResponse.exists?(email: message.to, response_type: :bounce) or EmailResponse.exists?(email: message.to, response_type: :complaint))
 			message.perform_deliveries = false
 			puts "Bounce should have happened"

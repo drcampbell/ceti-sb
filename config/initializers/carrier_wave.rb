dd4a9d9ea72944f8c6c1bd1dfb12229177311bce
David Campbell <d.cam09@gmail.com>
2015-09-09 09:30:24 -0400

    Change notificaitons to only send notification after creating, not initialization

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,32 +1,32 @@
-if false and Rails.env.production?
-  CarrierWave.configure do |config|
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-        # Configuration for Amazon S3
-        provider: 'AWS', 
-        access_key_id:      ENV['AWS_ACCESS_KEY'],
-        secret_access_key:  ENV['AWS_SECRET_KEY'],
-        region:             ENV['AWS_REGION']
-        #:host                   => ,
-        #:endpoint:key => "value" 
-    }
-    config.fog_directory = ENV['S3_BUCKET']
-  end
-end
-
-# if Rails.env.production?
+# if false and Rails.env.production?
 #   CarrierWave.configure do |config|
-#   	config.fog_provider = 'fog/aws'
+#     config.fog_provider = 'fog/aws'
 #     config.fog_credentials = {
 #         # Configuration for Amazon S3
-#         :provider              => 'AWS',
-#         :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-#         :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-#         :region								 => 'us-west-1'
-#         #:host									 => ,
+#         provider: 'AWS', 
+#         access_key_id:      ENV['AWS_ACCESS_KEY'],
+#         secret_access_key:  ENV['AWS_SECRET_KEY'],
+#         region:             ENV['AWS_REGION']
+#         #:host                   => ,
 #         #:endpoint:key => "value" 
 #     }
-#     config.fog_directory     = ENV['S3_BUCKET']
-#     config.fog_public				 = false
+#     config.fog_directory = ENV['S3_BUCKET']
 #   end
 # end
+
+if Rails.env.production?
+  CarrierWave.configure do |config|
+  	config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+        # Configuration for Amazon S3
+        :provider              => 'AWS',
+        :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+        :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+        :region								 => 'us-west-1'
+        #:host									 => ,
+        #:endpoint:key => "value" 
+    }
+    config.fog_directory     = ENV['S3_BUCKET']
+    config.fog_public				 = false
+  end
+end

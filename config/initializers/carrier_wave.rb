76eb06fb7c84d099368adab97db29c50a399e29e
David Campbell <d.cam09@gmail.com>
2015-09-09 09:49:00 -0400

    Somehow broke carrierwave and trying to fix

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,13 +4,15 @@ if Rails.env.production?
     config.fog_credentials = {
         # Configuration for Amazon S3
         provider: 'AWS', 
-        access_key_id:      ENV['AWS_ACCESS_KEY'],
-        secret_access_key:  ENV['AWS_SECRET_KEY'],
+        aws_access_key_id:      ENV['AWS_ACCESS_KEY'],
+        aws_secret_access_key:  ENV['AWS_SECRET_KEY'],
         region:             ENV['AWS_REGION']
         #:host                   => ,
         #:endpoint:key => "value" 
     }
     config.fog_directory = ENV['S3_BUCKET']
+    config.fog_public = false
+    
   end
 end
 

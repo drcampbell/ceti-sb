f9bc07e94902d2a5f42633970b2b50eff04c6dac
David Campbell <d.cam09@gmail.com>
2015-09-28 10:44:31 -0400

    upload changetype

--- app/controllers/api/schools_controller.rb
+++ app/controllers/api/schools_controller.rb
@@ -54,7 +54,7 @@ class API::SchoolsController < API::ApplicationController
                       phone: @school.phone}
     badge = Badge.find(@school.badge_id)
     events = Event.where(loc_id: @school.id).where("event_start > ?", Time.now).order("event_start").reverse
-    render json: {school: school_message, badge_url: badge.url, events: list_events(events).as_json}
+    render json: {school: school_message, badge_url: badge.file_name, events: list_events(events).as_json}
   end
 
   def choose

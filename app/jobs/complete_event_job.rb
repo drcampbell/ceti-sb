f993952ce1a2bfc5618fe326f3873085c9b87716
David Campbell <d.cam09@gmail.com>
2015-10-19 13:53:37 -0400

    Add initialization of complete to false for new events

--- app/jobs/complete_event_job.rb
+++ app/jobs/complete_event_job.rb
@@ -2,6 +2,6 @@ class CompleteEventJob < ActiveJob::Base
   queue_as :default
 
   def perform(*args)
-    Event.where('event_end < ?' Time.now).where('speaker_id != ?',0).where(:complete => true)
+    Event.where('event_end < ?', Time.now).where('speaker_id != ?',0).where(:complete => true)
   end
 end

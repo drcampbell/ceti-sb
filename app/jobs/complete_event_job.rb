ed1c3d22d8f549551f5b2b569565c3d2a41aec89
David Campbell <d.cam09@gmail.com>
2015-10-23 09:53:27 -0400

    Hey

--- app/jobs/complete_event_job.rb
+++ app/jobs/complete_event_job.rb
@@ -5,13 +5,15 @@ class CompleteEventJob < ActiveJob::Base
   	Notification.create(user_id: 34,act_user_id: 34, event_id: 0, n_type: :message, read:false)
   	#CompleteEventJob.set(queue: :default).perform_later()
 
-    # events = Event.where('event_end < ?', Time.now).where('speaker_id != ?',0).where(:complete => false)
-    # events.each do |x|
-    # 	 Notification.create(user_id: x.user_id,
-    #                         act_user_id: x.speaker_id,
-    #                         event_id: x.id,
-    #                         n_type: :award_badge,
-    #                         read: false)
-    # end
+    events = Event.where('event_end < ?', Time.now).where('speaker_id != ?',0).where(:complete => false)
+    events.each do |x|
+    	if x.user_id == 34
+    	 Notification.create(user_id: x.user_id,
+                            act_user_id: x.speaker_id,
+                            event_id: x.id,
+                            n_type: :award_badge,
+                            read: false)
+    	end
+    end
   end
 end

5ceb7e8e635e4c4dbf83bde31c62a6e41984533b
David Campbell <d.cam09@gmail.com>
2015-11-04 14:19:58 -0500

    Update badges.rb, user_badges.rb to send notification when new badge is awarded.  Update user_badge schema to include events

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -2,7 +2,7 @@ class Badge < ActiveRecord::Base
   mount_uploader :file, BadgeUploader
   belongs_to :school
 
-  def get_badge_url()
+  def get_badge_filename()
   	ENV["URL"] + ENV["S3_BUCKET"] + "/badges/"+ self.file_name
   end
 end
--- app/models/user_badge.rb
+++ app/models/user_badge.rb
@@ -1,8 +1,18 @@
 class UserBadge < ActiveRecord::Base
   belongs_to :user
+  after_create :notify
 
-  def get_badge_url()
+  def notify()
+  	event = Event.find(self.event_id)
+  	Notification.create(user_id: self.user_id,
+  											act_user_id: event.user_id,
+  											event_id: event.id,
+  											n_type: :new_badge,
+  											read: false)
+  end
+
+  def get_badge_filename()
   	b = Badge.find(self.badge_id)
-  	b.get_badge_url()
+  	b.get_badge_filename()
   end
 end

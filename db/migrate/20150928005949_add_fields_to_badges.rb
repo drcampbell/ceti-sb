9586b3a8a4c73f5f64af37ef7c56be19890be446
David Campbell <d.cam09@gmail.com>
2016-01-15 10:30:56 -0500

    Delete bad migration

--- db/migrate/20150928005949_add_fields_to_badges.rb
+++ db/migrate/20150928005949_add_fields_to_badges.rb
@@ -2,6 +2,6 @@ class AddFieldsToBadges < ActiveRecord::Migration
   def change
   	add_column :badges, :url, :string
   	add_column :badges, :file_name, :string
-  	add_column :badges, :e_type, :string
+  	add_column :badges, :f_type, :string
   end
 end
--- db/migrate/20150928032131_change_type.rb
+++
@@ -1,5 +0,0 @@
-class ChangeType < ActiveRecord::Migration
-  def change
-  	rename_column :badges, :type, :f_type
-  end
-end

713f72947fdc06668dcb947f7e95b47d4b14e0c6
David Campbell <d.cam09@gmail.com>
2016-01-26 15:58:00 -0500

    Update search vector

--- db/migrate/20160126201615_searchvectorforreal.rb
+++ db/migrate/20160126201615_searchvectorforreal.rb
@@ -1,13 +1,14 @@
 class Searchvectorforreal < ActiveRecord::Migration
   def up
     # Add search vector column
-    add_column :schools, :search_vector, 'tsvector'
+    add_column :schools, :search_vector, :tsvector
+    add_index :schools, :search_vector, using: "gin"
     # Add gin index on the search vector
-    execute <<-SQL
-      CREATE INDEX schools_search_idx
-      ON schools
-      USING gin(search_vector);
-    SQL
+    #execute <<-SQL
+      #CREATE INDEX schools_search_idx
+      #ON schools
+      #USING gin(search_vector);
+    #SQL
 
     # Trigger to update vector column when updated
     execute <<-SQL
@@ -24,9 +25,10 @@ class Searchvectorforreal < ActiveRecord::Migration
   end
   
   def down
-    remove_column :schools, :search_vector
     execute <<-SQL
       DROP TRIGGER IF EXISTS schools_search_vector_update on schools;
     SQL
+    remove_index :schools, :search_vector
+    remove_column :schools, :search_vector
   end
 end

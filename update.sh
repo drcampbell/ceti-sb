1dcd2c15993dca46ca494a4a9f3e52adbe9de377
David Campbell <d.cam09@gmail.com>
2016-03-07 14:17:59 -0500

    Updatin gupdater

--- update.sh
+++ update.sh
@@ -5,12 +5,11 @@ VERSION=$(perl -pe 's/^((\d+\.)*)(\d+)(.*)$/$1.($3+1).$4/e' < version)
 echo $VERSION > version
 
 # Create Git Tag
-git tag -a v$VERSION
-git push origin --tags
-git add version
-git commit -m "Update with Version"
-git push
+#git tag -a v$VERSION
+#git push origin --tags
+#git add version
+#git commit -m "Update with Version"
+#git push
 
-eb deploy ceti-test-env
 #git push
 

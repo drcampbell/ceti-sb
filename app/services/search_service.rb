d9701275f1289f8c2426c3999eedad4635a08e46
David Campbell <d.cam09@gmail.com>
2016-03-07 21:21:50 -0500

    Update with Version

--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -1,7 +1,20 @@
 class SearchService
+ 
+  def handle_abbr(query) 
+  state_to_abbr = {"Alaska"=>"AK", "Alabama"=>"AL", "Arkansas"=>"AR", "American Samoa"=>"AS", "Arizona"=>"AZ", "California"=>"CA", "Colorado"=>"CO", "Connecticut"=>"CT", "District of Columbia"=>"DC", "Delaware"=>"DE", "Florida"=>"FL", "Georgia"=>"GA", "Guam"=>"GU", "Hawaii"=>"HI", "Iowa"=>"IA", "Idaho"=>"ID", "Illinois"=>"IL", "Indiana"=>"IN", "Kansas"=>"KS", "Kentucky"=>"KY", "Louisiana"=>"LA", "Massachusetts"=>"MA", "Maryland"=>"MD", "Maine"=>"ME", "Michigan"=>"MI", "Minnesota"=>"MN", "Missouri"=>"MO", "Mississippi"=>"MS", "Montana"=>"MT", "North Carolina"=>"NC", "North Dakota"=>"ND", "Nebraska"=>"NE", "New Hampshire"=>"NH", "New Jersey"=>"NJ", "New Mexico"=>"NM", "Nevada"=>"NV", "New York"=>"NY", "Ohio"=>"OH", "Oklahoma"=>"OK", "Oregon"=>"OR", "Pennsylvania"=>"PA", "Puerto Rico"=>"PR", "Rhode Island"=>"RI", "South Carolina"=>"SC", "South Dakota"=>"SD", "Tennessee"=>"TN", "Texas"=>"TX", "Utah"=>"UT", "Virginia"=>"VA", "Virgin Islands"=>"VI", "Vermont"=>"VT", "Washington"=>"WA", "Wisconsin"=>"WI", "West Virginia"=>"WV", "Wyoming"=>"WY"} 
+    #state_to_abbr.keys.each do |st|
+      #if query.downcase.include? st.downcase
+        #query += " " + state_to_abbr[st]
+      #end
+    #end
+    return query
+  end
+
   def search(model, params)
+
     if params[:search]
-      @search = model.search_full_text(params[:search])
+      query = handle_abbr(params[:search])
+      @search = model.search_full_text(query)
       if model == Event
         @search = @search.reorder(event_start: :desc)
       end
